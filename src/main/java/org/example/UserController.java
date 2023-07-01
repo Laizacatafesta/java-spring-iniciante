a58ec88458a2104c471b2ff5844ec811a6420203
Laiza Catafesta <72575967+Laizacatafesta@users.noreply.github.com>
2023-07-01 10:58:39 -0300

    Criação de endpoint para busca de um usuário.

--- src/main/java/org/example/UserController.java
+++ src/main/java/org/example/UserController.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,12 @@ public class UserController {
     @GetMapping
     public List<User> findAll() {
         List<User> result = repository.findAll();
+        return result;
+    }
+
+    @GetMapping(value = "/{id}")
+    public User findById(@PathVariable Long id) {
+        User result = repository.findById(id).get();
         return result;
     }
 }
